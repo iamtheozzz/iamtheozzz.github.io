cda813dc1d861a6d4f39255e36643133dfa6cdcb
iamtheozzz <theozhang722@gmail.com>
2024-05-19 14:29:42 +0800

    some changes

--- clock/clock.js
+++ clock/clock.js
@@ -1,59 +1,125 @@
-var groups = document.querySelectorAll('.digit')
+const bars = [
+	['end', 'top'],
+	['side', 'top', 'left'],
+	['side', 'top', 'right'],
+	['middle'],
+	['side', 'bottom', 'left'],
+	['side', 'bottom', 'right'],
+	['end', 'bottom']
+];
 
-function setNumber(group, number) {
-	var clocks = group.children;
-	var numbers = [
-		'266352355555555551451664',
-		'263013500550055024131664',
-		'266316352645526451631664',
-		'266316352645163526451664',
-		'232355555145163500550014',
-		'266352645163163526451664',
-		'266352645163523551451664',
-		'266316350055005500550014',
-		'266352355145523551451664',
-		'266352355145163500550014'
-	]
-	for(var i = 0; i < 24; i++) {
-		clocks[i].classList.value = 'clock pos' + numbers[number][i]
+const $main = document.querySelector('main');
+
+const addDigits = number => {
+	const initGroup = (number, padding = 2) => {
+		const $group = document.createElement('div');
+		$group.classList.add('group');
+
+		const digits = [...`${number}`.padStart(padding, 0)].map(digit => {
+			const $digit = document.createElement('figure');
+			$digit.classList.add('digit');
+			$digit.setAttribute('data-digit', digit);
+			bars.forEach(classes => {
+				const $span = document.createElement('span');
+				$span.classList.add(...classes);
+				$digit.append($span);
+			});
+			return $digit;
+		});
+
+		$group.append(...digits);
+
+		return {
+			element: $group,
+			set number(val) {
+				number = val;
+				[...`${number}`.padStart(padding, 0).substring(`${number}`.length - 2)].forEach((digit, i) => {
+					digits[i].setAttribute('data-digit', digit);
+				});
+			},
+
+			get number() {
+				return number;
+			}
+		}
+	}
+	
+	const $digits = document.createElement('div');
+	$digits.classList.add('digits');
+	const group = initGroup(number);
+	const groupShadow1 = initGroup(number);
+	const groupShadow2 = initGroup(number);
+	groupShadow1.element.classList.add('shadow');
+	groupShadow1.element.classList.add('shadow1');
+	groupShadow2.element.classList.add('shadow');
+	groupShadow2.element.classList.add('shadow2');
+	$digits.append(group.element);
+	$digits.append(groupShadow1.element);
+	$digits.append(groupShadow2.element);
+	$main.append($digits);
+	
+	return {
+		set number(val) {
+			number = val;
+			group.number = val;
+			groupShadow1.number = val;
+			groupShadow2.number = val;
+		},
+		get number() {
+			return number;
+		}
 	}
 }
 
-function setSeparator(group) {
-	var pos = '002314231400';
-	for(var i = 0; i<12; i++) {
-		group.children[i].classList.value = 'clock pos' + pos[i];
+const addColon = () => {
+	const $colonGroup = document.createElement('div');
+	$colonGroup.classList.add('colon-group');
+	const $colon = document.createElement('figure');
+	$colon.append(document.createElement('span'));
+	const $colonShadow1 = document.createElement('figure');
+	$colonShadow1.append(document.createElement('span'));
+	const $colonShadow2 = document.createElement('figure');
+	$colonShadow2.append(document.createElement('span'));
+	$colon.classList.add('colon');
+	$colonShadow1.classList.add('colon', 'shadow', 'shadow1');
+	$colonShadow2.classList.add('colon', 'shadow', 'shadow2');
+	$colonGroup.append($colon);
+	$colonGroup.append($colonShadow1);
+	$colonGroup.append($colonShadow2);
+	$main.append($colonGroup);
+}
+
+const init = () => {
+	let now = new Date();
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+	
+	const numberHour = addDigits(hours);
+	addColon();
+	const numberMinute = addDigits(minutes);
+	addColon();
+	const numberSecond = addDigits(seconds);
+	
+	const update = () => {
+		now = new Date();
+		let newSeconds = now.getSeconds();
+		if (seconds !== newSeconds) {
+			hours = now.getHours();
+			minutes = now.getMinutes();
+			seconds = newSeconds;
+			numberHour.number = hours;
+			numberMinute.number = minutes;
+			numberSecond.number = seconds;
+		}
+		
+		requestAnimationFrame(update);
 	}
+	update();
 }
 
-function pad(number, digits) {
-  number = parseInt(number, 10)  
-	return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+if (/^(?:(?!chrome|android)[\s\S])*(?:safari|iPad|iPhone|iPod)/i.test(navigator.userAgent)) {
+	document.body.classList.add('safari');
 }
 
-function writeTime() {
-	var now = new Date();
-	var hour = now.getHours().toString();
-	var minute = now.getMinutes().toString();
-	var number = pad(hour,2) + pad(minute, 2);
-	setNumber(groups[0], number[0]);
-	setNumber(groups[1], number[1]);
-	setNumber(groups[3], number[2]);
-	setNumber(groups[4], number[3]);
-}
-
-function runEveryMinute(f) {
-	var now = new Date();
-	var timeUntilNextMinute = 60000 - (now.getSeconds() * 1000 + now.getMilliseconds());
-
-	setTimeout(function(){
-		f();
-		setInterval(f, 60000);
-	}, timeUntilNextMinute);
-}
-
-setTimeout(function() {
-	setSeparator(groups[2]);
-	runEveryMinute(writeTime);
-	writeTime();
-}, 200);
+init();
